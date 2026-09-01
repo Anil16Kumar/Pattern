6b5c019cf70ad10de7852bd96131f9c5bff48d06
Anil Kumar <anilkumarsingh0160@gmail.com>
2026-09-02 01:02:27 +0530

    initial commit for all pattern in DSA

---
+++ src/main/java/Two_Pointers/TwoSum.java
@@ -0,0 +1,52 @@
+package Two_Pointers;
+
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.Map;
+
+public class TwoSum {
+
+    //for un-sorted array:
+    public static int[] twoSum_unsorted(int[] nums, int target) {
+        Map<Integer, Integer> map = new HashMap<>();
+        for (int i = 0; i < nums.length; i++) {
+            int complement = target - nums[i];
+            if (map.containsKey(complement)) {
+                return new int[]{map.get(complement), i};
+            }
+            map.put(nums[i], i);
+        }
+        return new int[]{-1, -1};
+    }
+
+
+    // for sorted array:
+    public static int[] twoSum(int[] nums, int target) {
+        if (nums == null || nums.length == 0)
+            return new int[]{-1, -1};
+
+        int left = 0;
+        int right = nums.length - 1;
+
+        while (left < right) {
+            int sum = nums[left] + nums[right];
+            if (sum == target) {
+                return new int[]{left, right};
+            } else if (sum < target) {
+                left++;
+            } else {
+                right--;
+            }
+        }
+
+        return new int[]{-1, -1};
+    }
+
+    public static void main(String[] args) {
+        int[] nums = {2, 7, 11, 15};
+        int target = 9;
+        int[] result = twoSum(nums, target);
+        System.out.println(Arrays.toString(result));
+    }
+
+}
