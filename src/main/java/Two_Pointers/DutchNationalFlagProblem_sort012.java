29a8c30d2738e87317d7c1d9badab97edb0f476a
Anil Kumar <anilkumarsingh0160@gmail.com>
2026-09-02 20:17:47 +0530

    code practice for 2 pointer

--- src/main/java/Two_Pointers/DutchNationalFlagProblem_sort012.java
+++ src/main/java/Two_Pointers/DutchNationalFlagProblem_sort012.java
@@ -31,7 +31,6 @@ public class DutchNationalFlagProblem_sort012 {
         }
     }
 
-
     public static void main(String[] args) {
         Scanner scanner=new Scanner(System.in);
         int n=scanner.nextInt();
